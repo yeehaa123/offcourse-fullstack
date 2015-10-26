c62fc2d02b3c02b9150a5c871f5aa269ecc8d258
Jan Hein Hoogstad <yeehaa@codingthehumanities.com>
2015-10-26 09:41:57 +0100

    add collection model

--- src-cljs/offcourse/datastore/model.cljs
+++ src-cljs/offcourse/datastore/model.cljs
@@ -1,7 +1,6 @@
 (ns offcourse.datastore.model
   (:require [offcourse.models.action :refer [respond]]
-            [offcourse.models.course :as co]
-            [offcourse.models.checkpoint :as cp]))
+            [offcourse.models.collection :as cl]))
 
 (defrecord DataStore [collections courses])
 
@@ -11,20 +10,23 @@
 (defn update-collections [store collection-name collection-ids]
   (assoc-in store [:collections collection-name] collection-ids))
 
+(defn update-cache [store fn]
+  (update-in store [:courses] fn))
+
 (defn update-course [store course]
-  (assoc-in store [:courses (:id course)] course))
+  (update-cache store #(cl/update-course %1 course)))
 
 (defn update-courses [store courses]
-  (reduce update-course store courses))
+  (update-cache store #(cl/update-courses %1 courses)))
 
 (defn add-checkpoint [store course-id checkpoint]
-  (update-in store [:courses course-id] #(co/add-checkpoint %1 checkpoint)))
+  (update-cache store #(cl/add-checkpoint %1 course-id checkpoint)))
 
 (defn toggle-done [store course-id checkpoint-id]
-  (update-in store [:courses course-id] #(co/toggle-done %1 checkpoint-id)))
+  (update-cache store #(cl/toggle-done %1 course-id checkpoint-id)))
 
 (defn augment-checkpoint [store course-id checkpoint-id resource]
-  (update-in store [:courses course-id] #(co/augment-checkpoint %1 checkpoint-id resource)))
+  (update-cache store #(cl/augment-checkpoint %1 course-id checkpoint-id resource)))
 
 (defn find-course [store course-id]
-  (get (:courses store) course-id))
+  (find-course (:courses @store) course-id))
