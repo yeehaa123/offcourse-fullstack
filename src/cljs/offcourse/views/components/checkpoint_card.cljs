67eece0416c00860fcf5484fb8ebb424a23e2727
Jan Hein Hoogstad <yeehaa@codingthehumanities.com>
2015-11-04 15:24:31 +0100

    fix styling

--- src/cljs/offcourse/views/components/checkpoint_card.cljs
+++ src/cljs/offcourse/views/components/checkpoint_card.cljs
@@ -3,24 +3,26 @@
             [offcourse.views.components.temp-components :refer [Map Title Meta Tags CardSection GoToButton]]
             [offcourse.helpers.css :as css]))
 
-(defn CheckpointCard [{:keys [highlighted task id]}
-                      {:keys [course-id goal]}
-                      {:keys [url title content]}
-                      {:keys [go-to-checkpoint] :as handlers}
-                      in-sidebar?]
-  (let [highlighted (if highlighted "highlighted" "not-highlighted")
-        basic   [[:map (Map)]
-                 [:title (Title task)]
-                 [:meta (if in-sidebar?
-                          (Meta :title title
-                                :url url
-                                :goal goal)
-                          (Meta :title title
-                                :url url))]
-                 [:tags (Tags ["React" "Angular" "FrontEnd"])]]
-        extra    [:course-button (GoToButton course-id handlers)]
-        sections (if in-sidebar? (conj basic extra) basic)]
-    (d/section {:key id
-                :className (css/classes "card" highlighted)
-                :onClick #(go-to-checkpoint course-id id %1)}
-               (map-indexed #(CardSection %1 %2) sections))))
+(defn CheckpointCard
+  ([checkpoint course resource handlers] (CheckpointCard checkpoint course resource handlers false))
+  ([{:keys [highlighted task id]}
+    {:keys [course-id goal]}
+    {:keys [url title content]}
+    {:keys [go-to-checkpoint] :as handlers}
+    in-sidebar?]
+   (let [highlighted (if highlighted "highlighted" "not-highlighted")
+         basic   [[:map (Map)]
+                  [:title (Title task)]
+                  [:meta (if in-sidebar?
+                           (Meta :title title
+                                 :url url
+                                 :goal goal)
+                           (Meta :title title
+                                 :url url))]
+                  [:tags (Tags ["React" "Angular" "FrontEnd"])]]
+         extra    [:course-button (GoToButton course-id handlers)]
+         sections (if in-sidebar? (conj basic extra) basic)]
+     (d/section {:key id
+                 :className (css/classes "card" highlighted)
+                 :onClick #(go-to-checkpoint course-id id %1)}
+                (map-indexed #(CardSection %1 %2) sections)))))
