703e08dcf2d3189f86efb02c8d6c79ec3fd170a1
Jan Hein Hoogstad <yeehaa@codingthehumanities.com>
2015-11-11 18:09:06 +0100

    wip refactor appstate store

---
+++ src/cljs/offcourse/appstate/responder.cljs
@@ -0,0 +1,31 @@
+(ns offcourse.appstate.responder
+  (:require [offcourse.models.action :refer [respond]]))
+
+(def counter (atom 0))
+
+(defn respond-resource-required [[field {:keys [course collection]}]]
+  (swap! counter inc)
+  (let [resource-data (case field
+                        :collection-names {:type field}
+                        :tag-names {:type field}
+                        :user-names {:type field}
+                        :collection  {:type :collection
+                                      :collection collection}
+                        :courses     {:type :collection
+                                      :collection collection}
+                        :course    {:type :course
+                                    :course course})]
+    (when (< @counter 1000)
+      (respond :requested-data
+               :data resource-data))))
+
+(defn respond-update [appstate]
+  (respond :updated-appstate
+           :appstate appstate))
+
+(defn respond-commit [payload course-id checkpoint]
+  (let [payload (assoc payload :course-id course-id
+                       :checkpoint checkpoint)]
+    (respond :requested-commit
+             :payload payload)))
+
--- src/cljs/offcourse/appstate/store.cljs
+++ src/cljs/offcourse/appstate/store.cljs
@@ -1,77 +1,59 @@
 (ns offcourse.appstate.store
   (:require [offcourse.appstate.model :as model]
             [offcourse.appstate.viewmodel :as vm]
-            [offcourse.appstate.viewmodels.collection :as cl-vm]
-            [offcourse.appstate.viewmodels.course :as co-vm]
-            [offcourse.models.course :as co]
-            [offcourse.models.action :refer [respond]]
-            [cljs.core.match :refer-macros [match]]))
+            [offcourse.appstate.responder :as res]
+            [offcourse.models.course :as co]))
 
-(def counter (atom 0))
 (def appstate (atom (model/new-appstate)))
 
-(defn respond-resource-required [field {:keys [course collection]}]
-  (swap! counter inc)
-  (let [resource-data (case field
-                        :collection-names {:type field}
-                        :tag-names {:type field}
-                        :user-names {:type field}
-                        :collection  {:type :collection
-                                      :collection collection}
-                        :courses     {:type :collection
-                                      :collection collection}
-                        :course    {:type :course
-                                    :course course})]
-    (when (< @counter 1000)
-      (respond :requested-data
-               :data resource-data))))
-
-(defn- update-appstate! [fn]
-  (do
-    (swap! appstate fn)
-    (respond :updated-appstate
-             :appstate @appstate)))
-
-(defn- respond-appstate [appstate]
-  (let [[unknown-field viewmodel] (model/unknown-data appstate)]
+(defn update? []
+  (let [unknown-field (model/unknown-data @appstate)]
     (if unknown-field
-      (respond-resource-required unknown-field viewmodel)
-      (respond :updated-appstate
-               :appstate appstate))))
+      (res/respond-resource-required unknown-field)
+      (res/respond-update @appstate))))
 
-;; Public API
+(defn update! []
+  (res/respond-update @appstate))
 
 (defn set-mode [{mode :mode}]
-  (update-appstate! #(model/set-mode %1 mode)))
+  (do
+    (swap! appstate #(model/set-mode %1 mode))
+    (update!)))
 
 (defn toggle-mode []
-  (update-appstate! #(model/toggle-mode %1)))
+  (do
+    (swap! appstate #(model/toggle-mode %1))
+    (update!)))
+
+(defn set-user [{:keys [user-id]}]
+  (do
+    (swap! appstate #(model/set-user-id %1 user-id))
+    (update!)))
 
 (defn set-level [payload]
-  (let [appstate (swap! appstate #(model/set-level %1 payload))]
-    (respond-appstate appstate)))
+  (do
+    (swap! appstate #(model/set-level %1 payload))
+    (update?)))
 
 (defn refresh [{:keys [store] :as payload}]
-  (let [appstate (swap! appstate #(model/refresh %1 store))]
-    (respond-appstate appstate)))
+  (do
+    (swap! appstate #(model/refresh %1 store))
+    (update?)))
 
 (defn toggle-highlight [payload]
   (let [{type :type :as level} (:level @appstate)]
     (case type
-      :collection (update-appstate! #(model/highlight-collection %1 payload))
-      :course (update-appstate! #(model/highlight-course %1 payload)))))
+      :collection (do
+                    (swap! appstate #(model/highlight-collection %1 payload))
+                    (res/respond-update @appstate))
+      :course (do
+                (swap! appstate #(model/highlight-course %1 payload))
+                (res/respond-update @appstate)))))
 
 (defn commit-data [{:keys [course-id checkpoint-id] :as payload}]
   (let [course (:course (:viewmodel @appstate))
-        checkpoint (co/find-checkpoint course checkpoint-id)
-        payload (assoc payload :course-id course-id
-                               :checkpoint checkpoint)]
-    (respond :requested-commit
-             :payload payload)))
+        checkpoint (co/find-checkpoint course checkpoint-id)]
+    (res/respond-commit payload course-id checkpoint)))
 
 (defn force-refresh []
-  (respond :reloaded-appstate
-           :appstate @appstate))
-
-(defn set-user [{:keys [user-id]}]
-  (update-appstate! #(model/set-user-id %1 user-id)))
+  (res/respond-update appstate))
